bed5a3e85a0a47b6b646676cf2d854848e5bb396
Vigneswaran-Saravanan <s.vigneswaran007@gmail.com>
2026-06-15 23:11:02 -0400

    add my-matches route before id route to fetch player joined matches

--- src/routes/matchRoutes.js
+++ src/routes/matchRoutes.js
@@ -7,7 +7,8 @@ import {
   deleteMatch,
   joinMatch,
   confirmPlayer,
-  leaveMatch
+  leaveMatch,
+  getMyMatches
 } from '../controllers/matchController.js'
 import { protect, requireRole } from '../middleware/authmiddleware.js'
 
@@ -15,6 +16,9 @@ const router = Router()
 
 // GET /api/matches — get all matches 
 router.get('/', getMatches)
+
+// GET /api/matches/my-matches — get all matches the logged-in player joined
+router.get('/my-matches', protect, getMyMatches)
 
 // GET /api/matches/:id — get one match 
 router.get('/:id', getMatchById)
